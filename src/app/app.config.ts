6defee9682593bb17934c04c7c84b0dfc0ca359b
hhaoz <hao.tt03643@sinhvien.hoasen.edu.vn>
2025-02-26 11:33:00 +0700

    feat: open auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,28 +5,27 @@ import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideStore} from '@ngrx/store';
 import {provideEffects} from '@ngrx/effects';
-// import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
-// import {getAuth, provideAuth} from '@angular/fire/auth';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
 import {provideHttpClient} from '@angular/common/http';
 import {authReducer} from './ngrx/auth/auth.reducer';
-// import * as authEffects from './ngrx/auth/auth.effects';
+import * as authEffects from './ngrx/auth/auth.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}), provideHttpClient(), provideRouter(routes), provideAnimationsAsync(),
     provideStore({
       auth: authReducer
     }),
-    // provideEffects([
-    //   authEffects
-    // ]),
-    // provideFirebaseApp(() => initializeApp({
-    //   "projectId": "kanban-246",
-    //   "appId": "1:656199323172:web:b6a85c041e28e03a8b52b1",
-    //   "storageBucket": "kanban-246.firebasestorage.app",
-    //   "apiKey": "AIzaSyAy1F24MJYwj0d2Mya4T7fJgPG3wC4Lwv0",
-    //   "authDomain": "kanban-246.firebaseapp.com",
-    //   "messagingSenderId": "656199323172",
-    //   "measurementId": "G-14D94KVP60"
-    // })), provideAuth(() => getAuth())
-  ]
+    provideEffects([
+      authEffects
+    ]),
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "kanban-246",
+      "appId": "1:656199323172:web:b6a85c041e28e03a8b52b1",
+      "storageBucket": "kanban-246.firebasestorage.app",
+      "apiKey": "AIzaSyAy1F24MJYwj0d2Mya4T7fJgPG3wC4Lwv0",
+      "authDomain": "kanban-246.firebaseapp.com",
+      "messagingSenderId": "656199323172",
+      "measurementId": "G-14D94KVP60"
+    })), provideAuth(() => getAuth())]
 };
--- src/app/ngrx/auth/auth.effects.ts
+++ src/app/ngrx/auth/auth.effects.ts
@@ -1,32 +1,32 @@
-// import {Actions, createEffect, ofType} from '@ngrx/effects';
-// import {inject} from '@angular/core';
-// import {AuthService} from '../../services/auth/auth.service';
-// import * as authActions from './auth.actions';
-// import {catchError, map, of, switchMap} from 'rxjs';
-//
-// export const login$ = createEffect(
-//   (actions$ = inject(Actions), authService = inject(AuthService)) => {
-//     return actions$.pipe(
-//       ofType(authActions.login),
-//       switchMap(() => {
-//           return authService.signInWithGoogle().pipe(
-//             switchMap((accessToken) => {
-//                 return authService.login(accessToken).pipe(
-//                   map(() => authActions.loginSuccess({accessToken: accessToken})),
-//                   catchError((error) => {
-//                     return of(authActions.loginFailure({error: error.message}));
-//                   })
-//                 )
-//               }
-//             ),
-//             catchError((error) => {
-//               return of(authActions.loginFailure({error: error.message}));
-//             })
-//           );
-//         }
-//       )
-//     )
-//   }, {
-//     functional: true
-//   }
-// )
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {inject} from '@angular/core';
+import {AuthService} from '../../services/auth/auth.service';
+import * as authActions from './auth.actions';
+import {catchError, map, of, switchMap} from 'rxjs';
+
+export const login$ = createEffect(
+  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+    return actions$.pipe(
+      ofType(authActions.login),
+      switchMap(() => {
+          return authService.signInWithGoogle().pipe(
+            switchMap((accessToken) => {
+                return authService.login(accessToken).pipe(
+                  map(() => authActions.loginSuccess({accessToken: accessToken})),
+                  catchError((error) => {
+                    return of(authActions.loginFailure({error: error.message}));
+                  })
+                )
+              }
+            ),
+            catchError((error) => {
+              return of(authActions.loginFailure({error: error.message}));
+            })
+          );
+        }
+      )
+    )
+  }, {
+    functional: true
+  }
+)
